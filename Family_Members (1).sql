de121b8e30a91187020e5df3508204ebbe7dc858
arslnbatu <52040113+arslnbatu@users.noreply.github.com>
2020-01-06 01:28:50 +0300

    Add files via upload

---
+++ Family_Members (1).sql
@@ -0,0 +1,61 @@
+create table Family_Members (
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	DOB DATE,
+	Email VARCHAR(50),
+	Phone Number VARCHAR(50),
+	city VARCHAR(50),
+	Job title VARCHAR(50)
+);
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Andy', 'Leversuch', '3/20/1993', 'aleversuch0@nytimes.com', '797-798-5729', 'Kuwait', 'Occupational Therapist');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Tiler', 'Bathoe', '9/5/1993', 'tbathoe1@chron.com', '956-200-2026', 'Pakistan', 'Actuary');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Grazia', 'Kures', '12/26/1988', 'gkures2@princeton.edu', '686-308-8257', 'Afghanistan', 'Programmer II');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Free', 'Abden', '12/4/1966', 'fabden3@twitpic.com', '525-717-9377', 'Democratic Republic of the Congo', 'Associate Professor');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Farley', 'Orro', '1/16/1954', 'forro4@nsw.gov.au', '965-293-1853', 'Armenia', 'Community Outreach Specialist');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Madelin', 'Erskin', '6/9/2015', 'merskin5@illinois.edu', '233-984-3223', 'China', 'Junior Executive');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Corrianne', 'Jarnell', '6/19/2003', 'cjarnell6@csmonitor.com', '512-296-9729', 'Indonesia', 'Pharmacist');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Edvard', 'Spilsted', '1/17/1964', 'espilsted7@ovh.net', '164-595-0397', 'Indonesia', 'Health Coach IV');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Justino', 'Fearnyough', '10/18/1995', 'jfearnyough8@bloglovin.com', '432-906-9129', 'Tanzania', 'Administrative Officer');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Terri-jo', 'Papworth', '3/22/1982', 'tpapworth9@merriam-webster.com', '774-698-5375', 'China', 'Sales Associate');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Maritsa', 'Bewsey', '1/22/1978', 'mbewseya@google.de', '186-432-8779', 'Philippines', 'Director of Sales');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Aarika', 'Dorrell', '10/30/1979', 'adorrellb@google.co.uk', '703-834-9434', 'Russia', 'Graphic Designer');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Alida', 'Tiron', '5/13/1969', 'atironc@simplemachines.org', '290-293-4994', 'China', 'Business Systems Development Analyst');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Cecile', 'Rathke', '2/24/1988', 'crathked@joomla.org', '292-822-7984', 'France', 'Product Engineer');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Bettye', 'Middleditch', '8/20/1998', 'bmiddleditche@blogtalkradio.com', '241-662-1186', 'Japan', 'Civil Engineer');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Jephthah', 'Antczak', '12/5/1986', 'jantczakf@wikispaces.com', '673-377-5765', 'China', 'Tax Accountant');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Rustie', 'Greenwood', '10/24/1992', 'rgreenwoodg@smh.com.au', '301-933-6239', 'China', 'GIS Technical Architect');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Maddi', 'Beauvais', '5/18/1959', 'mbeauvaish@opensource.org', '609-106-1790', 'Czech Republic', 'Human Resources Manager');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Edi', 'Carlton', '8/21/2000', 'ecarltoni@ihg.com', '486-515-1809', 'Honduras', 'Project Manager');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Glenn', 'Bawle', '9/23/2003', 'gbawlej@chron.com', '555-809-9450', 'Russia', 'Help Desk Operator');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Antonino', 'Causton', '9/23/2003', 'acaustonk@cyberchimps.com', '953-580-9467', 'China', 'Help Desk Operator');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Cecelia', 'Rame', '3/1/2012', 'cramel@buzzfeed.com', '732-632-0203', 'China', 'Teacher');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Cassandra', 'Crome', '10/14/2009', 'ccromem@army.mil', '358-542-8426', 'China', 'Nurse Practicioner');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Haroun', 'Pyson', '2/20/2008', 'hpysonn@blogger.com', '671-580-0403', 'France', 'Marketing Assistant');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Aindrea', 'Cullen', '7/8/1987', 'aculleno@cbsnews.com', '129-584-5178', 'South Africa', 'Programmer Analyst I');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Wallache', 'Olivas', '4/27/1998', 'wolivasp@list-manage.com', '911-785-5319', 'Russia', 'Biostatistician II');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Arnoldo', 'Walliker', '7/19/1992', 'awallikerq@yolasite.com', '920-870-9124', 'China', 'Chief Design Engineer');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Creighton', 'Kestell', '4/1/1974', 'ckestellr@ibm.com', '351-842-8808', 'Indonesia', 'Junior Executive');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Farica', 'Dooher', '6/22/1977', 'fdoohers@oracle.com', '888-716-7323', 'Indonesia', 'Cost Accountant');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Sondra', 'Branton', '8/28/1995', 'sbrantont@freewebs.com', '922-676-4360', 'Bulgaria', 'Graphic Designer');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Kassandra', 'Mallalieu', '12/25/1956', 'kmallalieuu@digg.com', '387-625-1027', 'Lithuania', 'Budget/Accounting Analyst II');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Lazaro', 'Sissot', '9/9/1999', 'lsissotv@phoca.cz', '349-532-1864', 'Brazil', 'Financial Advisor');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Zuzana', 'Simecek', '3/2/1997', 'zsimecekw@squidoo.com', '690-962-0761', 'China', 'Financial Advisor');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Rodolfo', 'Blakemore', '6/16/1958', 'rblakemorex@chron.com', '794-342-8337', 'Indonesia', 'Programmer I');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Clevie', 'Greswell', '12/20/2000', 'cgreswelly@cbsnews.com', '427-635-4480', 'China', 'Desktop Support Technician');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Angeli', 'Medlen', '6/19/1980', 'amedlenz@arizona.edu', '190-240-3900', 'France', 'Occupational Therapist');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Josee', 'Wheeldon', '9/19/1977', 'jwheeldon10@google.co.jp', '219-575-4472', 'Poland', 'Health Coach II');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Nertie', 'Gronw', '5/4/1971', 'ngronw11@miitbeian.gov.cn', '503-900-0042', 'Indonesia', 'Structural Engineer');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Dannie', 'Woller', '9/25/2007', 'dwoller12@amazonaws.com', '383-873-2940', 'Russia', 'Statistician II');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Cybill', 'Collecott', '11/12/1999', 'ccollecott13@1688.com', '811-320-7631', 'China', 'Geological Engineer');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Myranda', 'Mulliss', '1/10/1996', 'mmulliss14@hexun.com', '681-448-9777', 'Poland', 'Database Administrator III');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Stafani', 'Groucutt', '10/18/2008', 'sgroucutt15@home.pl', '212-911-0860', 'Indonesia', 'Assistant Manager');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Abram', 'Trank', '12/14/1985', 'atrank16@intel.com', '236-291-0172', 'Indonesia', 'Electrical Engineer');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Jabez', 'Kirkup', '8/7/1960', 'jkirkup17@marketwatch.com', '473-510-3367', 'Kazakhstan', 'Director of Sales');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Sanders', 'Gerardet', '6/4/1988', 'sgerardet18@multiply.com', '780-900-2956', 'Greece', 'VP Marketing');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Lanette', 'Crossfield', '12/22/1968', 'lcrossfield19@lulu.com', '460-397-0396', 'Poland', 'Data Coordiator');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Winslow', 'Wanjek', '3/1/1951', 'wwanjek1a@google.pl', '693-552-5019', 'Macedonia', 'Pharmacist');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Anson', 'Quaife', '7/22/1986', 'aquaife1b@ow.ly', '133-153-5272', 'China', 'Junior Executive');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Danielle', 'Emberson', '5/5/1990', 'demberson1c@hubpages.com', '804-886-6546', 'Ghana', 'Executive Secretary');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Alric', 'Glewe', '2/19/1981', 'aglewe1d@jigsy.com', '818-462-1568', 'China', 'Software Consultant');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Sherye', 'Dimmick', '10/4/1956', 'sdimmick1e@economist.com', '891-295-3318', 'Sweden', 'Environmental Specialist');
+insert into Family_Members (first_name, last_name, DOB, Email, Phone Number, city, Job title) values ('Olwen', 'Sissel', '10/19/2016', 'osissel1f@yahoo.com', '211-981-0236', 'France', 'Sales Associate');
